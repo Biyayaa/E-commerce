b97c0073924ed42629f950d2291db9a402ab5503
Biyayaa <onaleye.ap@gmail.com>
2023-04-25 16:58:07 +0100

    adding carting system

--- script.js
+++ script.js
@@ -1,89 +1,62 @@
-let productsArray = [];
+let myGoods;
+let url = "https://dummyjson.com/products";
+let products = document.getElementById("products")
+async function fetchGoods(){
+    let goods = await fetch(url);
+    let res = await goods.json();
+    localStorage.setItem("goods", JSON.stringify(res.products))
+}
+fetchGoods();
 
-async function fetchProducts() {
-  try {
-    let response = await fetch("https://dummyjson.com/products");
-    let data = await response.json();
-    console.log(data);
-    
-    let dummyJsonProducts = Object.values(data); // store all products in the array
-    console.log(dummyJsonProducts);
+myGoods = JSON.parse(localStorage.getItem("goods"));
+console.log(myGoods);
+let myCart = JSON.parse(localStorage.getItem("cart")) || [];
+console.log(myCart);
 
-    productsArray = productsArray.concat(dummyJsonProducts); // merge with the existing products array
-
-    let productsElement = document.getElementById("products");
-    productsElement.innerHTML = ""; // clear any existing content
-    
-    data.products.forEach((item) => {
-      productsElement.innerHTML += `
-
-      <div class="products">
-      
-      <div onclick="productItem(${item.id})" class="pro-container" >
-          <div class="pro">
-          <img src="${item.thumbnail}">
-              <div class="des">
-                  <span>${item.brand}</span>
-                  <h5>${item.title}</h5>
-                  <h4>$${item.price}</h4>
-                  <span class="rate">${item.rating}</span>
-              </div>
-              <i class="fa-solid fa-cart-plus"></i>
-          </div>
-
+async function displayGoods(){
+    let resp = await myGoods.forEach((el, index)=>{
+    let errmm = myCart.some(ssmm => ssmm.id == el.id);
+    products.innerHTML += `
+            <div class=' col-md-3 m-1 card shadow w-25'>
+                <img src=${el.thumbnail} w-75 />
+                <p>${el.description}</p>
+                <button onclick="addToCart(event,${el.id})" class='btn btn-warning'>${errmm? "Remove from Cart": "Add to Cart"}</button>
             </div>
-      `;
-    });
-  } catch (error) {
-    console.error(error);
-  }
+        `
+    })
+}
+displayGoods();
+
+function addToCart(ev, id){
+    let found = myGoods.find(el=> el.id == id)
+    console.log(found);
+    console.log(myCart);
+    let errmm = myCart.some(ssmm => ssmm.id == found.id);
+    console.log(errmm);
+    if(errmm){
+        ev.target.innerHTML = "Not there... Add";
+        let myIndex = myCart.indexOf(found)
+        console.log(myIndex);
+        myCart.splice(myIndex,1)
+        localStorage.setItem("cart", JSON.stringify(myCart));
+        cartCount();
+        return;
+    }else{
+        ev.target.innerHTML = "Already there... Remove"
+        myCart.push(found);
+        localStorage.setItem("cart", JSON.stringify(myCart));
+        cartCount();
+    }
+    console.log(myCart);
+    displayGoods();
 }
 
-async function fetchProductsS() {
-  try {
-    let response = await fetch("https://fakestoreapi.com/products");
-    let data = await response.json();
-    console.log(data);
-    
-    let fakeStoreProducts = Object.values(data); // store all products in the array
-    console.log(fakeStoreProducts);
-
-    productsArray = productsArray.concat(fakeStoreProducts); // merge with the existing products array
-
-    let productsElement = document.getElementById("products2");
-    productsElement.innerHTML = ""; // clear any existing content
-    
-    data.forEach((item) => {
-      productsElement.innerHTML += `
-            <div class="pro-container">
-                <div class="pro">
-                <img src="${item.image}">
-                    <div class="des">
-                        <span>${item.brand}</span>
-                        <h5>${item.title}</h5>
-                        <h4>$${item.price}</h4>
-                        <span>${item.rating.rate}</span>
-                    </div>
-                    <i class="fa-solid fa-cart-plus"></i>
-                </div>
-
-            </div>
-
-      `;
-    });
-  } catch (error) {
-    console.error(error);
-  }
+function cartCount(){
+    document.getElementById("cart-count").innerHTML = myCart.length;
 }
+cartCount();
 
-// Call both functions to fetch products from the two APIs
-fetchProducts();
-fetchProductsS();
-
-
-console.log(productsArray);
-
-let productItem = document.getElementById("productItem");
-productItem.addEventListener("click", ()=>{
-  console.log("works");
-})
+function showOne(id){
+    localStorage.setItem("oneItem", id);
+    window.location.href = "oneProd.html"
+}
