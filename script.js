709a4c7b88aade2e1679787af11ec0be7745a1d6
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-04-28 17:12:41 +0100

    added flutterwave payment

--- script.js
+++ script.js
@@ -191,3 +191,34 @@ function showOne(id) {
   localStorage.setItem("oneItem", id);
   window.location.href = "oneProd.html";
 }
+
+function makePayment() {
+  let total = 0;
+  myCart.forEach((item) => {
+    total += item.price * item.quantity;
+  });
+  let cartTotal = document.getElementById("cartTotal");
+  cartTotal.innerHTML = `${total.toFixed(2)}`;
+  FlutterwaveCheckout({
+    public_key: "FLWPUBK_TEST-1c1913436f4bb40793ad01c221cffc6f-X",
+    tx_ref: "titanic-48981487343MDI0NzMx",
+    amount: total,
+    currency: "USD",
+    payment_options: "card, mobilemoneyghana, ussd",
+    redirect_url: "https://glaciers.titanic.com/handle-flutterwave-payment",
+    meta: {
+      consumer_id: 23,
+      consumer_mac: "92a3-912ba-1192a",
+    },
+    customer: {
+      email: "rose@unsinkableship.com",
+      phone_number: "08102909304",
+      name: "Rose DeWitt Bukater",
+    },
+    customizations: {
+      title: "Biyaya Store",
+      description: "Payment for an awesome cruise",
+      logo: "https://www.logolynx.com/images/logolynx/22/2239ca38f5505fbfce7e55bbc0604386.jpeg",
+    },
+  });
+}
